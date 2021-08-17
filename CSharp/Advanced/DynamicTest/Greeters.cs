8f3426dd7303d70691161aa99fde6755ef5e0d58
km-hussain-in <km.hussain.in@gmail.com>
2021-08-17 14:00:51 +0000

    Code

--- CSharp/Advanced/DynamicTest/Greeters.cs
+++ CSharp/Advanced/DynamicTest/Greeters.cs
@@ -1,15 +1,44 @@
-namespace DynamicTest
+namespace Greeting
 {
-	class CasualGreeter
-	{
-		public string Meet(string visitor) => $"Hi {visitor}";
+	using System;
+	using System.Dynamic;
 
-		public string Leave(string visitor) => $"Bye {visitor}";
-
-	}
-	
 	class FormalGreeter
 	{
-		public string Meet(string visitor) => $"Hello {visitor}";	
+		public string Title { get; set; } = "Mx";
+
+		public DateTime Time => DateTime.Now;
+
+		public string Meet(string visitor) => $"Hello {Title} {visitor}";	
+	}
+
+	class CasualGreeter : DynamicObject
+	{
+		public int Code => Environment.TickCount; 
+
+		public String Greeted { get; set; } = "Nobody";
+
+		public int Count { get; set; } = 0;
+
+		public string Meet(string visitor)
+		{
+			Greeted = visitor;
+			Count += 1;
+			return $"Hi {visitor}";
+		}
+
+		public override bool TryInvokeMember(InvokeMemberBinder method, object[] arguments, out object result)
+		{
+			if(method.Name == "Leave" && arguments.Length == 1 && Equals(arguments[0], Greeted))
+			{
+				result = $"Bye {Greeted}";
+				return true;
+			}
+			result = null;
+			return false;
+		}
+
 	}
 }
+
+
--- CSharp/Advanced/DynamicTest/Program.cs
+++ CSharp/Advanced/DynamicTest/Program.cs
@@ -2,35 +2,25 @@ using System;
 
 namespace DynamicTest
 {
+	
 	class Program
 	{
-		static dynamic CreateEmployee(int hours, float rate)
-		{
-			dynamic employee = new System.Dynamic.ExpandoObject();
-			employee.Hours = hours;
-			employee.Rate = rate;
-			employee.Income = (Func<double>)(() => employee.Hours * employee.Rate);
-			if(hours > 180)
-				employee.Bonus = (Func<float, double>)(p => 0.01 * p * employee.Rate * (employee.Hours - 180));
-			return employee;
-		}
-
 		static void Main(string[] args)
 		{
 			try
 			{	
-				Type t = Type.GetType($"DynamicTest.{args[0]}", true);
-				dynamic g = Activator.CreateInstance(t); //DLR call-site will be created for g to support duck typing
-				Console.WriteLine(g.Meet("Jack"));
-				if(args.Length > 2)
+				Type t = Type.GetType(args[0], true);
+				object g = Activator.CreateInstance(t); 
+				foreach(var prop in t.GetProperties())
 				{
-					int h = int.Parse(args[1]);
-					float r = float.Parse(args[2]);
-					dynamic emp = CreateEmployee(h, r);
-					Console.WriteLine("Income: {0:0.00}", emp.Income());
-					Console.WriteLine("Bonus : {0:0.00}", emp.Bonus(40));
+					Console.WriteLine($"{prop.Name} = {prop.GetValue(g)}");
 				}
-				Console.WriteLine(g.Leave("Jack"));
+				dynamic dg = g;
+				Console.WriteLine(dg.Meet("Jack"));
+				if(args.Length > 1)
+				{
+					Console.WriteLine(dg.Leave(args[1]));
+				}
 			}
 			catch(Exception ex)
 			{
