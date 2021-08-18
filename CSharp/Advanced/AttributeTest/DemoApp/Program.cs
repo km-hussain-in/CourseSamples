05feb9c49fe438b078672da13654ca4425e0c2e8
km-hussain-in <km.hussain.in@gmail.com>
2021-08-18 04:12:40 +0000

    Code

--- CSharp/Advanced/AttributeTest/DemoApp/Program.cs
+++ CSharp/Advanced/AttributeTest/DemoApp/Program.cs
@@ -1,4 +1,4 @@
-using Finance;
+using Finance.Loans;
 using System;
 using System.Reflection;
 
@@ -11,15 +11,15 @@ namespace DemoApp
 		static void Main(string[] args)
 		{
 			double p = double.Parse(args[0]);
-			Type t = Type.GetType("Finance." + args[1] + ",FinLib");
+			Type t = Type.GetType("Finance.Loans." + args[1] + ",Finance", true);
 			object policy = Activator.CreateInstance(t);
-			var op = t.GetMethod(args.Length < 3 ? "GetInterestRate" : "GetInterestRateFor" + args[2]);
-			var mda = op.GetCustomAttribute<MaxDurationAttribute>();
-			int m = mda?.Limit ?? 10;
-			var rate = op.CreateDelegate<RateFunc>(policy);
+			MethodInfo option = t.GetMethod(args.Length < 3 ? "Common" : args[2]);
+			var mda = option.GetCustomAttribute<MaxDurationAttribute>();
+			int m = mda?.Limit ?? 6;
+			var interest = option.CreateDelegate<RateFunc>(policy);
 			for(int n = 1; n <= m; ++n)
 			{
-				float i = rate(p, n) / 1200;
+				float i = interest(p, n) / 1200;
 				double e = p * i / (1 - Math.Pow(1 + i, -12 * n));
 				Console.WriteLine("{0, -6}{1, 12:0.00}", n, e);
 			}
