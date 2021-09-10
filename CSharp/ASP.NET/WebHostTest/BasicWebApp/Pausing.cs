6c2bee38206f03fe023d754ebbcfe5fc56748917
km-hussain-in <km.hussain.in@gmail.com>
2021-09-10 03:38:03 +0000

    Code

--- CSharp/ASP.NET/WebHostTest/BasicWebApp/Pausing.cs
+++ CSharp/ASP.NET/WebHostTest/BasicWebApp/Pausing.cs
@@ -8,23 +8,23 @@ namespace BasicWebApp
 	public class Pausing
 	{
 		private readonly RequestDelegate _next;
-		private readonly int pause;
-		private long lastTime;
+		private readonly TimeSpan pause;
+		private DateTime lastTime;
 
 		public Pausing(RequestDelegate next, IConfiguration config)
 		{
 			_next = next;
-			pause = config.GetValue<int?>("Pausing:Interval") ?? 10; //see appsettings.json
+			pause = config.GetValue<TimeSpan?>("Pausing:Interval") ?? TimeSpan.FromSeconds(10); //see appsettings.json
 		}
 
 		public async Task Invoke(HttpContext context)
 		{
-			long thisTime =  DateTime.Now.Ticks / TimeSpan.TicksPerSecond;
-			if(thisTime - lastTime < pause)
-				await context.Response.WriteAsync($"Busy, please try after {pause - thisTime + lastTime} sec...");
+			var delta = lastTime + pause - DateTime.Now;
+			if(delta.Ticks > 0)
+				await context.Response.WriteAsync($"Busy, please try after {delta.Seconds} sec...");
 			else
 			{
-				lastTime = thisTime;
+				lastTime = DateTime.Now;
 				await _next.Invoke(context);
 			}
 		}
